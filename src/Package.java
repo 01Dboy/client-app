8e78070e3fa54b081fa813701c195862f10c52ce
01Dboy <oldboymailbox@gmail.com>
2025-08-31 20:10:07 +0300

    Вынес создание сигнатуры из класса Package в отдельный класс.

--- src/Package.java
+++ src/Package.java
@@ -1,47 +1,15 @@
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Random;
-import java.util.Set;
-
 public class Package implements Runnable {
-
-    private static Set<String> usedSignatures = Collections.synchronizedSet(new HashSet<>());
-    private static final String CHAR = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
-    private final Random random = new Random();
 
     @Override
     public void run() {
 
-        String signature = getSignature();
         UserInput userInput = new UserInput();
+
+        String signature = PackageSignature.getSignature();
         String dataType = dataType(userInput.call());
 
     }
 
-    private String generateSignature(){
-
-        StringBuffer signature = new StringBuffer(8);
-
-        for (int i = 0; i < 8; i++) {
-            signature.append(CHAR.charAt(random.nextInt(CHAR.length())));
-        }
-
-        return signature.toString();
-
-    }
-
-    private String getSignature() {
-
-        String signature;
-
-        do {
-            signature = generateSignature();
-        } while (!usedSignatures.add(signature));
-
-
-        return signature;
-
-    }
 
     private String dataType (int number) {
 
